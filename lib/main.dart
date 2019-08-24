422d53df44d84a7ebccdba0957614bcc63e8b63d
Daniel H. Morgan <dhmorgan@protonmail.com>
2019-08-24 18:11:26 -0500

    build release apk for testing install process

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(DosNWhos());
 
-class MyApp extends StatelessWidget {
+class DosNWhos extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
